bb0ddd64c36e2984d9c52bd165b4e0ddf2e9caf7
Juan Velasquez <valenelnoobatoyt@gmail.com>
2024-03-14 18:45:25 -0300

    Intento del ejercicio 3

--- lab-01/palabras.c
+++ lab-01/palabras.c
@@ -4,7 +4,14 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
-    
+    int c;
+    while((c = getchar()) != EOF && c != "^C") {
+    	if(c == " "){
+            printf("%c\n", c);
+ 	} else {
+	    printf("%c", c);
+	}        
+    }     
     
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
