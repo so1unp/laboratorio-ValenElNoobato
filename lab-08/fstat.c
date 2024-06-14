f784f68db746f7211e5cd185c3f737346e79dfa0
Juan Velasquez <valenelnoobatoyt@gmail.com>
2024-06-14 03:40:20 -0300

    Finalizacion del Lab 8

--- lab-08/fstat.c
+++ lab-08/fstat.c
@@ -1,10 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
-{
-    // Agregar código aquí.
+void print_file_info(const char *path) {
+    struct stat fileStat;
 
-    // Termina la ejecución del programa.
+    if (stat(path, &fileStat) < 0) {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
+
+    // ID del usuario propietario
+    printf("ID del usuario propietario: %d\n", fileStat.st_uid);
+
+    // Tipo de archivo y modo
+    printf("Tipo de archivo: ");
+    switch (fileStat.st_mode & S_IFMT) {
+        case S_IFBLK:  printf("block device\n");            break;
+        case S_IFCHR:  printf("character device\n");        break;
+        case S_IFDIR:  printf("directory\n");               break;
+        case S_IFIFO:  printf("FIFO/pipe\n");               break;
+        case S_IFLNK:  printf("symlink\n");                 break;
+        case S_IFREG:  printf("regular file\n");            break;
+        case S_IFSOCK: printf("socket\n");                  break;
+        default:       printf("unknown?\n");                break;
+    }
+
+    // Número de i-nodo
+    printf("Número de i-nodo: %ld\n", (long)fileStat.st_ino);
+
+    // Tamaño en bytes
+    printf("Tamaño en bytes: %lld\n", (long long)fileStat.st_size);
+
+    // Número de bloques que ocupa en disco
+    printf("Número de bloques: %lld\n", (long long)fileStat.st_blocks);
+
+    // Tiempo de última modificación
+    printf("Última modificación: %s", ctime(&fileStat.st_mtime));
+
+    // Tiempo de último acceso
+    printf("Último acceso: %s", ctime(&fileStat.st_atime));
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s <ruta_del_archivo>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    print_file_info(argv[1]);
+
     exit(EXIT_SUCCESS);
 }
--- lab-08/write_bytes.c
+++ lab-08/write_bytes.c
@@ -3,12 +3,17 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <time.h>
 
 int main(int argc, char *argv[])
 {
     size_t bufSize, numBytes, thisWrite, totWritten;
     char *buf;
     int sync, fd, openFlags;
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
 
     if (argc != 5) {
         fprintf(stderr, "Uso: %s num-bytes buf-size sync archivo\n", argv[0]);
@@ -33,16 +38,27 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    // Esta condicion esta presente para evitar el warning que dice que sync no esta siendo usada.
-    // Se puede eliminar cuando se implemente fsync() y fdatasync()
-    sync = sync == 1 ? 0 : 1;
-
     for (totWritten = 0; totWritten < numBytes; totWritten += thisWrite) {
         thisWrite = numBytes - totWritten > bufSize ? bufSize : numBytes - totWritten;
 
-        if (write(fd, buf, thisWrite) != thisWrite) {
-            perror("write");
-            exit(EXIT_FAILURE);
+        if (sync == 0) {
+            if (write(fd, buf, thisWrite) != thisWrite) {
+                perror("write");
+                exit(EXIT_FAILURE);
+            }
+        }
+        // Realizar la operación de sincronización según el valor de sync
+        if (sync == 1) {
+            if (fsync(fd) == -1) {
+                perror("fsync");
+                exit(EXIT_FAILURE);
+            }
+        } 
+        if (sync == 2) {
+            if (fdatasync(fd) == -1) {
+                perror("fdatasync");
+                exit(EXIT_FAILURE);
+            }
         }
     }
 
@@ -50,5 +66,10 @@ int main(int argc, char *argv[])
         perror("close");
         exit(EXIT_FAILURE);
     }
+
+    end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("El programa tardo %f segundos en ejecutarse\n", cpu_time_used);
     exit(EXIT_SUCCESS);
 }
